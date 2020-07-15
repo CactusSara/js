920c19f50c51fc7a3b9518370b2e74c4ad7b6d57
CactusSara <mnatsakanyansara@gmail.com>
2020-07-15 23:44:12 +0400

    homework

---
+++ index.js
@@ -0,0 +1,3 @@
+let first = prompt('Please, write the first number');
+let second = prompt('Please, write the second number');
+alert(`${parseInt(first) + parseInt(second)} is the sum of those two numbers!`)
